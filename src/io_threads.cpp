260fce30fe9093e7d55089226d07c26ede046a12
Whale Mo <ncwhale@gmail.com>
2019-01-31 23:12:58 +0800

    Remove unused comments.

--- src/io_threads.cpp
+++ src/io_threads.cpp
@@ -8,12 +8,7 @@ void ContextThreads::start(std::size_t thread_count = 1) {
   ctx->restart();
   for (std::size_t i = 0; i < thread_count; ++i) {
     threads.push_back(std::thread([this] {
-      // auto count =
       ctx->run();
-      // TODO: Use logger for these output.
-      // std::cout << "IO Thread RUN " << count
-      //           << " Times, Exit: " << std::this_thread::get_id() <<
-      //           std::endl;
     }));
   }
 }
