989d397d8fb1e4f036a597e10e7b2e75319aa2a6
whalemo(莫金雨) <whalemo@tencent.com>
2019-03-06 19:02:01 +0800

    Add timing count for fiber test.

--- test/test_fiber_threads.cpp
+++ test/test_fiber_threads.cpp
@@ -57,12 +57,20 @@ int main(int argc, char const *argv[]) {
   auto thread_count = std::thread::hardware_concurrency();
   ft.init(thread_count);
 
-  for (char c : std::string("abcdefghijklmnopqrstuvwxyz")) {
-    boost::fibers::fiber(boost::fibers::launch::post, [c]() { whatevah(c); })
-        .detach();
-    ++fiber_count;
+  auto start_point = std::chrono::steady_clock::now();
+  {
+    lock_type lk(mtx_count);
+    for (char c : std::string("abcdefghijklmnopqrstuvwxyz")) {
+      boost::fibers::fiber(boost::fibers::launch::post, [c]() { whatevah(c); })
+          .detach();
+      ++fiber_count;
+    }
   }
-
+  {
+    std::ostringstream oss;
+    oss << "Fibers init done! total :" << fiber_count << "\n";
+    std::cout << oss.str() << std::flush;
+  }
   boost::fibers::fiber([] {
     {
       lock_type lk(mtx_count);
@@ -74,5 +82,16 @@ int main(int argc, char const *argv[]) {
       .detach();
 
   ft.join();
+  auto end_point = std::chrono::steady_clock::now();
+  {  // Output process timing.
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+        end_point - start_point);
+    std::chrono::duration<double, std::milli> d_duration =
+        end_point - start_point;
+    std::ostringstream oss;
+    oss << "Jobs done in " << duration.count() << "ms.\n";
+    oss << "or " << d_duration.count() << "ms.";
+    std::cout << oss.str() << std::endl;
+  }
   return EXIT_SUCCESS;
 }
